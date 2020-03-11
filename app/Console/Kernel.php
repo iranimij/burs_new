fbead5b019f1c97af7ea6634a4f0dd236874e4dc
iranimij <iranimij@gmail.com>
2020-03-11 22:07:57 +0330

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -106,8 +106,8 @@ class Kernel extends ConsoleKernel
                 }
             }
             //this is time for run code
-//        })->dailyAt('08:00');
-        })->everyMinute();
+        })->dailyAt('08:00');
+//        })->everyMinute();
 
 
 
